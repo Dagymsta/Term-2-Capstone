e7c70cf05ea851794b748a4bfe1cd6a906524b67
Brett Wheeler <b.wheeler1214@yahoo.com>
2022-12-15 15:38:29 -0700

    finalcommit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,37 @@
-import { NavLink} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
+import "../styles/navbar.scss";
+import "../styles/home.scss";
+import "../styles/footer.scss";
+import "../styles/products.scss";
+import "../styles/contact.scss";
+import "../styles/cartitems.scss";
+import "../styles/cart.scss";
+import "../styles/singlemerchitem.scss";
+import "../styles/about.scss";
 
-
-import "../styles/navbar.scss"
-import "../styles/home.scss"
-import "../styles/footer.scss"
-import "../styles/products.scss"
-import "../styles/contact.scss"
-import "../styles/cartitems.scss"
-import "../styles/cart.scss"
-import "../styles/singlemerchitem.scss"
-import "../styles/about.scss"
 export default function Home() {
-
-    return(
+  return (
     <div className="page-container">
-
-        <div className="main-page-content">
-
-            <div className="clothing-ad-container">
-                <div className="image-left">
-                    <div className="advertisement">
-                        <div className="ad-button-wrapper">
-                            <NavLink className="buttons" to="/products">Products</NavLink>
-
-                            <NavLink className="buttons" to="/contact">Contact Us</NavLink>
-
-                            <NavLink className="buttons" to="/about">About</NavLink>
-
-                        </div>
-                    </div>
-                </div>
+      <div className="main-page-content">
+        <div className="clothing-ad-container">
+          <div className="image-left">
+            <div className="advertisement">
+              <div className="ad-button-wrapper">
+                <NavLink className="buttons" to="/products">
+                  Products
+                </NavLink>
+                <NavLink className="buttons" to="/contact">
+                  Contact Us
+                </NavLink>
+                <NavLink className="buttons" to="/about">
+                  About
+                </NavLink>
+              </div>
             </div>
-        </div>  
+          </div>
+        </div>
+      </div>
     </div>
-    )
- }
+  );
+}
